c2646861f5b1a8ef22fdd9763159b58aff8b50ff
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2023-10-04 21:12:18 -0300

    chore: setup develop environment with air and docker-compose

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,8 +9,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello World!")
 }
 
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello!")
+}
+
 func main() {
   http.HandleFunc("/", handler)
+  http.HandleFunc("/hello", HelloHandler)
   http.ListenAndServe(":8080", nil)
 }
 
--- dev/terminal.sh
+++ dev/terminal.sh
@@ -4,3 +4,7 @@ go mod init github.com/RonaldoSetzer/todo-api-go
 // Docker
 docker build -t ronaldosetzer/todo-api-go .
 docker run -p 8080:8080 ronaldosetzer/todo-api-go
+
+// Docker compose
+docker-compose build
+docker-compose up dev-app
