3ef7e31b6dd481a557486b82355421f31fcf1d9b
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2023-10-09 20:40:26 -0300

    feat: setup docker to handle with postgres and flyway

--- dev/terminal.sh
+++ dev/terminal.sh
@@ -14,6 +14,12 @@ docker run -p 8080:8080 ronaldosetzer/todo-api-go
 docker-compose build
 docker-compose up dev-app
 
+// Docker container ID = 0c9b4f19f842 exec the postgres
+docker exec -it 0c9b4f19f842 sh
+
+//docker remove all images
+docker rmi $(docker images -a -q)
+
 // Curls
 curl -X POST \
   -H "Content-Type: application/json" \
@@ -22,11 +28,18 @@ curl -X POST \
 
 curl -X GET http://localhost:8081/todos
 
-curl -X GET http://localhost:8081/todos/{uuid}
-
-curl -X DELETE http://localhost:8081/todos/{uuid}
+curl -X GET http://localhost:8081/todos/240a368c-1c46-4a2f-a048-2f740ce2baa7
 
 curl -X PUT \
   -H "Content-Type: application/json" \
   -d '{"title":"Updated todo", "description":"Updated todo description", "status":"done"}' \
   http://localhost:8081/todos/240a368c-1c46-4a2f-a048-2f740ce2baa7"
+
+
+curl -X PUT \ 
+  -H "Content-Type: application/json" \
+  -d '{"title":"Updated todo", "description":"Updated todo description", "status":"DONE"}' \
+  http://localhost:8081/todos/195ef1d3-bc6d-432c-b65f-69db59d59031
+
+//connect to a postgres with psql
+psql -h localhost -p 5432 -U postgres -d todo-api-go
---
+++ flyway/sql/V1__create_todo_table.sql
@@ -0,0 +1,10 @@
+-- V1__create_todo_table.sql
+CREATE TABLE IF NOT EXISTS todo (
+    id uuid PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status VARCHAR(10) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
